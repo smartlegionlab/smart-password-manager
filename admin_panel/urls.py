5a90bb86207617b6b84d03e2ff80ccf143bc91e1
A.A. Suvorov <smartlegiondev@gmail.com>
2025-09-01 09:14:21 +0700

    feat(admin_panel): add system info to the admin panel

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from admin_panel.views.panel import admin_panel_view
 from admin_panel.views.site_config.site_config_detail import site_config_detail_view
 from admin_panel.views.site_config.site_config_update import site_config_update_view
+from admin_panel.views.system_info.system_info_detail import system_info_detail_view
+from admin_panel.views.system_info.system_info_update import system_info_update_view
 
 app_name = 'admin_panel'
 
@@ -15,4 +17,10 @@ urlpatterns = [
 urlpatterns += [
     path('site-config/', site_config_detail_view, name='site_config'),
     path('site-config/update/', site_config_update_view, name='site_config_update'),
+]
+
+# System info
+urlpatterns += [
+    path('system-info/', system_info_detail_view, name='system_info'),
+    path('system-info/update/', system_info_update_view, name='system_info_update'),
 ]
